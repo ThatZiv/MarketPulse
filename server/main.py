8c03f4415578687137f6bf3b46905a3a92bfde08
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-01-23 16:42:17 -0500

    Connect through sql alchemy may an extra dependency in flask-sqlalchemy

--- server/main.py
+++ server/main.py
@@ -3,24 +3,22 @@ from dotenv import load_dotenv
 import os
 from supabase import create_client, Client
 import flask_jwt_extended as jw
-from flask_cors import CORS
+from flask_cors import CORS\
+
+from sqlalchemy import create_engine
 
 
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+#supabase: Client = create_client(url, key)
+
+
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
-    jwt = jw.JWTManager()
-    jwt.init_app(app)   
-
-    app.secret_key = os.getenv('TEST_KEY')
-    
-
+  
     from routes.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
@@ -29,12 +27,28 @@ def create_app():
 
 if __name__ == '__main__':  
     app = create_app()
-    @app.route('/api/private')
-    @jw.jwt_required()
-    def private_route():
-        current_user = jw.get_jwt_identity()
-  
+    CORS(app)
+    jwt = jw.JWTManager()
+    jwt.init_app(app) 
 
-        return 'message: This is a private route'
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
 
+    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+    engine = create_engine(DATABASE_URL)
+
+    try:
+        with engine.connect() as connection:
+            print("Connection successful!")
+    except Exception as e:
+        print(f"Failed to connect: {e}")
+
+    #@app.route('/')
+    #def route():
+    #    return 'hello'
+    
     app.run(debug=True, host='0.0.0.0')
