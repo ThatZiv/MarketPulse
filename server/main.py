dd055c5ea1220eb4a8ca8fb047f436582b7efb03
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-01-23 12:27:42 -0500

    dependencies for running authenticated routes installed and set up

--- server/main.py
+++ server/main.py
@@ -1,10 +1,22 @@
 from flask import Flask, session, redirect, url_for, request
 from dotenv import load_dotenv
 import os
+from supabase import create_client, Client
+import flask_jwt_extended as jw
+from flask_cors import CORS
+
+
 load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+    jwt = jw.JWTManager()
+    jwt.init_app(app)   
 
     app.secret_key = os.getenv('TEST_KEY')
     
@@ -17,7 +29,12 @@ def create_app():
 
 if __name__ == '__main__':  
     app = create_app()
-    # @app.route("/")
-    # def hello_world():
-    #     return "index page"
+    @app.route('/api/private')
+    @jw.jwt_required()
+    def private_route():
+        current_user = jw.get_jwt_identity()
+  
+
+        return 'message: This is a private route'
+
     app.run(debug=True, host='0.0.0.0')
