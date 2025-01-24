271e5b8d08db578823b67276fc462f279e9a055a
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-01-24 11:38:45 -0500

    Testing

---
+++ server/database/test.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine,  Column, Integer, String
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import pytest
+import psycopg2
+
+
+
+#Base = declarative_base()
+
+#class User(Base):
+#    __tablename__ = 'users'
+#
+#    id = Column(Integer, primary_key = True)
+#    firstname = Column(String)
+#    lastname = Column(String)
+
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
