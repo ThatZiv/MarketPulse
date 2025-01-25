4d9dfb299c1867b45595a0f1e1ea3ebf6e2344b9
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-01-25 01:12:47 -0500

    Fixed and added a test case

--- client/src/__test__/App.test.tsx
+++ client/src/__test__/App.test.tsx
@@ -1,14 +1,32 @@
 // src/__ tests __/App.test.tsx
 
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "@/pages/Login";
+import Create from "@/pages/Create";
+import { BrowserRouter } from "react-router-dom";
 
 test("demo", () => {
   expect(true).toBe(true);
 });
+// Needed to wrap in Router since it gets router App.tsx
+test("Renders the login page", async () => {
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
 
-test("Renders the login page", () => {
-  const { getByText } = render(<Login />);
-  expect(getByText("Email")).toBeInTheDocument();
+  const email = await screen.findByText("Email");
+  expect(email).toBeInTheDocument();
+});
+
+test("Renders the create user page", async () => {
+  render(
+    <BrowserRouter>
+      <Create />
+    </BrowserRouter>
+  );
+  const email = await screen.findByText("Email");
+  expect(email).toBeInTheDocument();
 });
