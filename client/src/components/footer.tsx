abd4578aa74e12bdec53d8e2383254d85be1891c
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2025-01-31 02:17:32 -0500

    footer styling fix

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const items = ['Docs', 'Contact Us', 'About Us', 'FAQs'];
 export function Footer() {
     return (
-        <div className='flex items-center justify-center mb-5'>
+        <div className='flex items-center justify-center pb-5 text-white'>
             <ul className='grid grid-cols-4 gap-5'>
                 {items.map((item, index) => (
                     <li key={index}>{item}</li> //will convert to links later or change the style entirely. This 
