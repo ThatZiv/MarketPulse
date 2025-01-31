27b065ae33410091c41e8130fbdb78e6eed50df2
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-01-30 19:17:18 -0500

    Removed a div from dashboard.tsx

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -23,51 +23,49 @@ export default function Dashboard() {
   );
 
   return (
-    <div>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  {paths
-                    .map((path, index) => {
-                      if (path === "") {
-                        return null;
-                      }
-                      if (index === paths.length - 1) {
-                        return (
-                          <BreadcrumbItem key={index}>
-                            <BreadcrumbPage>{path}</BreadcrumbPage>
-                          </BreadcrumbItem>
-                        );
-                      }
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {paths
+                  .map((path, index) => {
+                    if (path === "") {
+                      return null;
+                    }
+                    if (index === paths.length - 1) {
                       return (
                         <BreadcrumbItem key={index}>
-                          <BreadcrumbLink
-                            href={paths.slice(0, index + 1).join("/")}
-                          >
-                            {path}
-                          </BreadcrumbLink>
-                          <>
-                            <BreadcrumbSeparator className="hidden md:block" />
-                          </>
+                          <BreadcrumbPage>{path}</BreadcrumbPage>
                         </BreadcrumbItem>
                       );
-                    })
-                    .filter(Boolean)}
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-          </header>
-          <div className="flex items-center justify-center h-full">
-            <Outlet />
+                    }
+                    return (
+                      <BreadcrumbItem key={index}>
+                        <BreadcrumbLink
+                          href={paths.slice(0, index + 1).join("/")}
+                        >
+                          {path}
+                        </BreadcrumbLink>
+                        <>
+                          <BreadcrumbSeparator className="hidden md:block" />
+                        </>
+                      </BreadcrumbItem>
+                    );
+                  })
+                  .filter(Boolean)}
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </div>
+        </header>
+        <div className="flex items-center justify-center h-full">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
