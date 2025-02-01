b77659f85c0b18370f0172cd8d52cd1cbc13cc21
Zavaar Shah <thatziv@gmail.com>
2025-02-01 17:29:24 -0500

    fix nav user sidebar bg

--- client/src/components/nav-user.tsx
+++ client/src/components/nav-user.tsx
@@ -32,7 +32,7 @@ export function NavUser() {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent bg-slate-100 data-[state=open]:text-sidebar-accent-foreground"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar />
               <div className="grid flex-1 text-left text-sm leading-tight">
