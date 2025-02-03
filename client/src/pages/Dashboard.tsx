918fc62bde30c09e18f88c04dfa98099e634b5ac
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2025-02-02 22:58:10 -0500

    Fixed my dashboard issues.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -28,19 +28,13 @@ export default function Dashboard() {
     [location.pathname]
   );
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" onClick={toggleSidebar}/>
+            <SidebarTrigger className="-ml-1"/>
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
