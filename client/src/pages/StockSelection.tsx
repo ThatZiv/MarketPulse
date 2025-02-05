53dde5a7702a706a039203e15cc1a8536adf6f00
Zavaar Shah <thatziv@gmail.com>
2025-02-04 19:19:58 -0500

    stock selection button consistency

--- client/src/pages/StockSelection.tsx
+++ client/src/pages/StockSelection.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useSupabase } from "@/database/SupabaseProvider";
 import { useNavigate } from "react-router-dom";
-import { ArrowRight, ChevronDown } from "lucide-react";
+import { ArrowLeft, ArrowRight, ChevronDown } from "lucide-react";
 import useAsync from "@/hooks/useAsync";
 import { type Stock } from "@/types/stocks";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -107,7 +107,7 @@ export default function StockPage() {
       error: (err) => `Failed to save stock data: ${err.message}`,
     });
 
-    navigate("/");
+    navigate("/", { replace: false });
   };
 
   // Handle input field changes
@@ -256,17 +256,18 @@ export default function StockPage() {
           <div className="flex flex-col md:flex-row gap-3 md:gap-0 justify-between mt-8">
             <Button
               type="button"
-              className="px-10 py-5 rounded-full text-lg font-bold shadow-md transform hover:scale-105 active:scale-95 hover:bg-primary/60 active:bg-primary/70 transition-all duration-200"
+              className="flex items-center justify-center hover:bg-primary/60 active:bg-primary/70 disabled:opacity-50 w-full sm:w-auto"
               onClick={() => navigate("/")}
             >
-              Return
+              <ArrowLeft className="hidden md:block" />
+              Back
             </Button>
             <Button
               type="submit"
-              className="px-10 py-5 rounded-full text-lg font-bold shadow-md transform hover:scale-105 active:scale-95 flex items-center justify-center hover:bg-primary/60 active:bg-primary/70 transition-all duration-200 disabled:opacity-50 w-full sm:w-auto"
+              className="flex items-center justify-center hover:bg-primary/60 active:bg-primary/70 disabled:opacity-50 w-full sm:w-auto"
               disabled={stocksLoading}
             >
-              Submit <ArrowRight className="hidden md:block ml-2" />
+              Submit <ArrowRight className="hidden md:block" />
             </Button>
           </div>
         </form>
