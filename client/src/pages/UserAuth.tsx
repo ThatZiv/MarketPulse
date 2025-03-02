4de23435d508c3e3fb67f34583dc6477316e164e
Paarth Desai <hd0747@wayne.edu>
2025-03-02 15:05:15 -0500

    Added logo to the login page.

--- client/src/pages/UserAuth.tsx
+++ client/src/pages/UserAuth.tsx
@@ -3,22 +3,31 @@ import { LoginForm } from "@/components/login-form";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { useState } from "react";
 import { CreateForm } from "@/components/create_user_form";
+
 export default function UserAuth() {
   const [pageState, setPageState] = useState("login"); // "login" or "signup"
 
   const togglePageState = () => {
     setPageState((prevMode) => (prevMode === "login" ? "signup" : "login"));
   };
+
   return (
     <div className="m-0 p-0 min-h-screen bg-center bg-no-repeat bg-cover bg-stocks-graph dark:bg-dark-stocks-graph">
-      <div className="flex items-center justify-left text-3xl font-bold text-transparent p-2">
-        <span className="text-white ml-5 pt-2 mb-5">MarketPulse</span>
+      <div className="flex items-center justify-left text-5xl font-bold text-transparent p-2">
+        <div className="ml-5 pt-2 mb-5">
+          <span className="text-white">MarketPulse</span>
+          <img
+            src="/public/images/MarketPulse_Logo.png"
+            alt="MarketPulse Logo"
+            className="h-48 w-48 mt-5"
+          />
+        </div>
         <div className="absolute right-0 top-0 p-2">
           <ModeToggle />
         </div>
       </div>
       <div
-        className="md:max-w-lg max-w-xs mx-auto pb-8 "
+        className="md:max-w-lg max-w-xs mx-auto pb-8"
         style={{
           transform: `rotateY(${pageState === "login" ? 0 : 180}deg)`,
           transitionDuration: "750ms",
