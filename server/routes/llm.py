cf58f5da26e6db13e3e8c03d2e971419a64f6d28
Ian Crowe-Sittig <ft5139@wayne.edu>
2025-03-03 12:08:02 -0500

    Added if else if stock_purchases exist

--- server/routes/llm.py
+++ server/routes/llm.py
@@ -6,12 +6,12 @@ from flask import Blueprint, Response, request
 from langchain_community.llms import LlamaCpp
 # pylint: enable=no-name-in-module
 # pylint: disable=line-too-long
-# pylint: disable=not-callable
 # from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
-from database.tables import Stocks, Stock_Info, Stock_Predictions
+from database.tables import Stocks, Stock_Info, Stock_Predictions, User_Stock_Purchases
+from engine import get_engine
 from engine import get_engine
 
 llm_bp = Blueprint('llm', __name__, url_prefix='/llm')
@@ -63,6 +63,7 @@ def llm__stock_route():
         print("LLM_MODEL_PATH file does not exist. \
             Please download a gguf model from https://huggingface.co/models")
         return Response(status=500)
+    current_user = jw.get_jwt_identity()
     ticker = request.args.get('ticker')
 
     if not ticker:
@@ -98,20 +99,43 @@ def llm__stock_route():
         predictions = select(Stock_Predictions).where(Stock_Predictions.stock_id == output_id.stock_id).order_by(Stock_Predictions.created_at.desc()).limit(1)
         pred_output = session.connection().execute(predictions).first()
 
+        user_info = select(User_Stock_Purchases).where(User_Stock_Purchases.stock_id == output.stock_id).where(User_Stock_Purchases.user_id == current_user)
+        user_output = session.connection().execute(user_info).all()
+        count = 0
+        stocks_owned = 0
+        average = 0
+
         session.close()
 
         if not output or not pred_output:
             return "</think>\nMissing context for suggestion"
 
+        
         closing = output.stock_close
         closing_pred = json.loads(pred_output.model_1)
         model_pred = closing_pred['forecast'][0]
         model_pred_2 = closing_pred['forecast'][6]
     else :
         return "</think>\nMissing context for suggestion"
-    query_template = f"Hello, I currently have shares of {ticker} stock. \
+
+    if user_output:
+        for row in user_output:
+            count+=1
+            stocks_owned += row.amount_purchased
+            average += row.amount_purchased*row.price_purchased
+        if stocks_owned > 0:
+            average = average/stocks_owned
+
+        query_template = f"Hello, I currently have shares of {ticker} stock. \
+        I bought them for {average} dollars per share.\
         The current price is {closing} dollars.\
         I predict that tomorows price will be {model_pred} and next weeks will be {model_pred_2}. What should I do?\n<think>\n"
+    
+    else:
+        query_template = f"Hello, I currently have shares of {ticker} stock. \
+            The current price is {closing} dollars.\
+            I predict that tomorows price will be {model_pred} and next weeks will be {model_pred_2}. What should I do?\n<think>\n"
+    
     query = query_template.format(stocks=stocks, ticker=ticker)
     def generate_response():
         """ stream llm response """
