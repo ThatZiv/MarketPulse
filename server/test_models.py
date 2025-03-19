5c05f7067a0f3fdab30737984c59ddc89d7e87e0
IanCro12 <156384531+IanCro12@users.noreply.github.com>
2025-03-18 22:47:51 -0400

    Update server/test_models.py
    Co-authored-by: Zavaar Shah <35277113+ThatZiv@users.noreply.github.com>

--- server/test_models.py
+++ server/test_models.py
@@ -21,7 +21,7 @@ def test_attention_lstm():
     model.train(copy.deepcopy(data))
     output = model.run(copy.deepcopy(data), 7)
 
-    assert isinstance(output[0], float)
+    assert all(isinstance(val, float) for val in output)
 
 # Testing CNNLSTMTransformer
 def cnnlstmtransformer():
