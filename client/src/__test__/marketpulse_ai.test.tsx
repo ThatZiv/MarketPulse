ed88dff4fd7c670ddd5e1d0b4e436754643ac5f7
IanCro12 <ft5139@wayne.edu>
2025-03-24 17:21:43 -0400

    Started on AI test

--- client/src/__test__/marketpulse_ai.test.tsx
+++ client/src/__test__/marketpulse_ai.test.tsx
@@ -1,4 +1,4 @@
-import { ResetPassword } from "@/components/forgot-password";
+
 import { describe } from "node:test";
 import {
   act,
@@ -12,56 +12,78 @@ import "@testing-library/jest-dom";
 
 import { GenerateStockLLM } from "@/components/llm/stock-llm";
 
+
+
 afterEach(() => {
+  jest.restoreAllMocks();
   cleanup();
 });
-const mockSupabase = jest.fn(() => {
-  return { error: null };
-});
-const mockSignUpNewUser = jest.fn();
-const mockSignInWithEmail = jest.fn();
-const mockSignInWithGoogle = jest.fn();
-const mockSignOut = jest.fn();
-const mockStatus = jest.fn();
-const mockUser = jest.fn();
-const mockName = jest.fn();
-const mockAccount = jest.fn();
-const mockSession = jest.fn();
-jest.mock("@/database/SupabaseProvider", () => ({
-  useSupabase: () => {
-    return {
-      supabase: {
-        auth: {
-          resetPasswordForEmail: mockSupabase,
-        },
-      },
-      signUpNewUser: mockSignUpNewUser,
-      signInWithEmail: mockSignInWithEmail,
-      signInWithGoogle: mockSignInWithGoogle,
-      signOut: mockSignOut,
-      status: mockStatus,
-      user: mockUser,
-      displayName: mockName,
-      account: mockAccount,
-      session: mockSession,
-    };
-  },
-}));
+
+
 jest.mock("lucide-react", () => ({
     Moon: () => "MoonIcon",
     Sun: () => "SunIcon",
     Eye: () => "EyeIcon",
     EyeOff: () => "EyeOffIcon",
+    ChevronDown: () => "ChevronDown",
+    Loader2: () => "Loader2"
   }));
 
 jest.mock("@/lib/ApiProvider", () => ({
-    useApi: () => jest.fn()
+    useApi: () => {
+            return {getStockLlmOutput: jest.fn(() => {return "</think>Hello"})}}
+    } 
+))
+
+jest.mock("react-router", () => ({
+  Link: jest.fn()
 }))
 
+//jest.mock("@/components/ui/accordion", () => ({
+//  Accordion: jest.fn(),
+//  AccordionContent: jest.fn(),
+//  AccordionItem: jest.fn(),
+//  AccordionTrigger: jest.fn(),
+//}))
+
+
+//import axios, { type AxiosInstance, type AxiosError } from "axios";
+
+
+
 describe("MarketPulse AI", () => {
+  beforeAll(() => {
+    global.matchMedia = jest.fn().mockImplementation(query => ({
+      matches: false, 
+      media: query,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),}));
+
+  });
+  
     test("Render", async () => {
         const ticker = "test"
         render(<GenerateStockLLM ticker={ticker} />);
-    
+        const open = await screen.findByText("MarketPulse AI");
+        expect(open).toBeInTheDocument();
+        expect(open).not.toBeDisabled();
+
+        expect(open).toBeVisible();
+
+        await act(async () => {
+          await fireEvent.click(open);
+        });
+
+        const close = await screen.findByText("Close");
+        expect(close).toBeInTheDocument();
+
+        const think = await screen.findByText("Thinking...ChevronDown");
+        expect(think).toBeInTheDocument();
+
+        await act(async () => {
+          await fireEvent.click(think);
+        });
+        
+        const text_test = await screen.findByText("Hello");
       });
 });
