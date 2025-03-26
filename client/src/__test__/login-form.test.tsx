ded63d0d721ac00836c4dfc048a4bbc4e62867ea
Paarth Desai <hd0747@wayne.edu>
2025-03-26 19:42:46 -0400

    Completed the login form integration testing.

---
+++ client/src/__test__/login-form.test.tsx
@@ -0,0 +1,129 @@
+import { LoginForm } from "@/components/login-form";
+import { describe, test, afterEach, beforeEach } from "@jest/globals";
+import {
+  render,
+  screen,
+  fireEvent,
+  act,
+  cleanup,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+
+
+const mockSignInWithEmail = jest.fn();
+const mockSignInWithGoogle = jest.fn();
+const mockTogglePageState = jest.fn();
+const mockResetPasswordState = jest.fn();
+
+jest.mock("@/database/SupabaseProvider", () => ({
+  useSupabase: () => ({
+    signInWithEmail: mockSignInWithEmail,
+    signInWithGoogle: mockSignInWithGoogle,
+  }),
+}));
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllMocks();
+});
+
+describe("LoginForm Component", () => {
+  beforeEach(() => {
+    render(
+      <LoginForm
+        togglePageState={mockTogglePageState}
+        resetPasswordState={mockResetPasswordState}
+      />
+    );
+  });
+
+  test("renders LoginForm correctly", () => {
+    const title = screen.getByText("Log in");
+    const description = screen.getByText(
+      "Enter your info below to login to your account"
+    );
+    const emailInput = screen.getByPlaceholderText("Email Address");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const loginButton = screen.getByText("Login");
+    const forgotPasswordButton = screen.getByText("Forgot Password?");
+    const createAccountButton = screen.getByText("Create Account");
+
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(forgotPasswordButton).toBeInTheDocument();
+    expect(createAccountButton).toBeInTheDocument();
+  });
+
+  test("handles email and password input", async () => {
+    const emailInput = screen.getByPlaceholderText("Email Address");
+    const passwordInput = screen.getByPlaceholderText("Password");
+
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+      fireEvent.change(passwordInput, { target: { value: "password123" } });
+    });
+
+    expect(emailInput).toHaveValue("test@example.com");
+    expect(passwordInput).toHaveValue("password123");
+  });
+
+  test("calls signInWithEmail on login button click", async () => {
+    const emailInput = screen.getByPlaceholderText("Email Address");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const loginButton = screen.getByText("Login");
+
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+      fireEvent.change(passwordInput, { target: { value: "password123" } });
+      fireEvent.click(loginButton);
+    });
+
+    expect(mockSignInWithEmail).toHaveBeenCalledTimes(1);
+    expect(mockSignInWithEmail).toHaveBeenCalledWith(
+      "test@example.com",
+      "password123"
+    );
+  });
+
+  test("calls resetPasswordState on forgot password button click", async () => {
+    const forgotPasswordButton = screen.getByText("Forgot Password?");
+
+    await act(async () => {
+      fireEvent.click(forgotPasswordButton);
+    });
+
+    expect(mockResetPasswordState).toHaveBeenCalledTimes(1);
+    expect(mockResetPasswordState).toHaveBeenCalledWith(true);
+  });
+
+  test("calls togglePageState on create account button click", async () => {
+    const createAccountButton = screen.getByText("Create Account");
+
+    await act(async () => {
+      fireEvent.click(createAccountButton);
+    });
+
+    expect(mockTogglePageState).toHaveBeenCalledTimes(1);
+  });
+
+  test("toggles password visibility", async () => {
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const toggleVisibilityButton = screen.getByText("Eye Icon");
+
+    expect(passwordInput).toHaveAttribute("type", "password");
+
+    await act(async () => {
+      fireEvent.click(toggleVisibilityButton);
+    });
+    expect(passwordInput).toHaveAttribute("type", "text");
+
+    await act(async () => {
+      fireEvent.click(toggleVisibilityButton);
+    });
+    expect(passwordInput).toHaveAttribute("type", "text");
+  });
+});
