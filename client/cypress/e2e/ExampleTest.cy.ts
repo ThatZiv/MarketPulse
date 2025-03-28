64ab6a1e087b9134162dd15ab90e99be2dafc998
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2025-03-28 19:30:46 -0400

    added cypress to ci/cd pipeline and wrote a basic rendering test

--- client/cypress/e2e/ExampleTest.cy.ts
+++ client/cypress/e2e/ExampleTest.cy.ts
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('system test 1', () => {
+  it('rendering the components of the login page', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('div').should('contain', 'Login');
+    cy.get('div').contains('Enter your info below to login to your account').should('exist');
+    cy.get('div').contains("Don't have an account? Create Account").should('exist');
+    cy.get('div').contains('Login').should('exist');
+    cy.get('button').contains('Forgot Password?').should('exist');
+    cy.get('button[type="submit"]').should('contain', 'Login');
+    cy.get('div').find('input[placeholder="Email Address"]').should('exist');
+    cy.get('div').find('input[placeholder="Password"]').should('exist');
   })
 })
