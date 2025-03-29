b8065db88fb914e7f9603fa2b73aa9f1fb12654c
Paarth Desai <hd0747@wayne.edu>
2025-03-29 14:23:11 -0400

    Create account page render test

---
+++ client/src/__test__/create_form.test.tsx
@@ -0,0 +1,57 @@
+import { CreateForm } from "@/components/create_user_form";
+import { describe, test, afterEach, beforeEach } from "@jest/globals";
+import {
+  render,
+  screen,
+  fireEvent,
+  act,
+  cleanup,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+
+
+const mockSignUpNewUser = jest.fn();
+const mockSignInWithGoogle = jest.fn();
+const mockTogglePageState = jest.fn();
+
+jest.mock("@/database/SupabaseProvider", () => ({
+  useSupabase: () => ({
+    signUpNewUser: mockSignUpNewUser,
+    signInWithGoogle: mockSignInWithGoogle,
+  }),
+}));
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllMocks();
+});
+
+describe("CreateForm Component", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="g_id_onload"></div>
+      <div class="g_id_signin"></div>
+    `;
+    render(<CreateForm togglePageState={mockTogglePageState} />);
+  });
+
+  test("renders CreateForm correctly", () => {
+    const title = screen.getByText("Create an Account");
+    const description = screen.getByText(
+      "Enter email and password for your account"
+    );
+    const emailInput = screen.getByPlaceholderText("Email Address");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirm Password");
+    const createButton = screen.getByText("Create");
+
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(confirmPasswordInput).toBeInTheDocument();
+    expect(createButton).toBeInTheDocument();
+  });
+
+});
