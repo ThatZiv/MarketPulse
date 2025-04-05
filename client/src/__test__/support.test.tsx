94ee0fc366b3d8302fa853f2183bfcf91bdbffd1
Paarth Desai <hd0747@wayne.edu>
2025-04-05 15:39:29 -0400

    Fixed lint error

--- client/src/__test__/support.test.tsx
+++ client/src/__test__/support.test.tsx
@@ -3,13 +3,11 @@ import { describe, test, afterEach, beforeEach } from "@jest/globals";
 import {
   render,
   screen,
-  fireEvent,
-  act,
+
   cleanup,
 } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
-import { toast } from "sonner";
 
 jest.mock("sonner", () => ({
     toast: {
