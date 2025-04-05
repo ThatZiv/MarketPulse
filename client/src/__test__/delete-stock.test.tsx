2fa3e7556f31e2173eb21c59c75b2eb8737e18f1
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2025-04-05 19:11:02 -0400

    prettier fix

--- client/src/__test__/delete-stock.test.tsx
+++ client/src/__test__/delete-stock.test.tsx
@@ -1,117 +1,124 @@
 import { DeleteStock } from "../components/delete-stock";
 import { describe, test, beforeEach } from "@jest/globals";
-import {
-    render,
-    screen,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 import { Toaster } from "sonner";
 
 jest.mock("lucide-react", () => ({
-    AlertCircle: () => <span>AlertCircle Icon</span>,
+  AlertCircle: () => <span>AlertCircle Icon</span>,
 }));
-jest.mock('@/database/SupabaseProvider', () => ({
-    useSupabase: () => ({
-        user: { id: 'test-user-id', email: 'test@example.com' },
-        supabase: {
-            from: jest.fn().mockReturnValue({
-                delete: jest.fn().mockReturnValue({
-                    match: jest.fn().mockResolvedValueOnce({ data: [], error: null })
-                }),
-            }),
-        },
-    }),
+jest.mock("@/database/SupabaseProvider", () => ({
+  useSupabase: () => ({
+    user: { id: "test-user-id", email: "test@example.com" },
+    supabase: {
+      from: jest.fn().mockReturnValue({
+        delete: jest.fn().mockReturnValue({
+          match: jest.fn().mockResolvedValueOnce({ data: [], error: null }),
+        }),
+      }),
+    },
+  }),
 }));
 
 const queryClient = new QueryClient();
 
 describe("DeleteStock Component", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <DeleteStock ticker="Tesla" stock_id={1} />
+          <Toaster />
+        </QueryClientProvider>
+      </MemoryRouter>
+    );
+  });
+  test("renders delete stock button correctly", () => {
+    const deleteButton = screen.getByRole("button", { name: /Delete/i });
+    expect(deleteButton).toBeInTheDocument();
+  });
+  test("renders alert dialog correctly", async () => {
+    const deleteButton = screen.getByRole("button", { name: /Delete/i });
+    expect(deleteButton).toBeInTheDocument();
+    await userEvent.click(deleteButton);
+    expect(
+      await screen.findByText(
+        "Are you absolutely sure you want to delete the entire stock history?"
+      )
+    ).toBeInTheDocument();
+  });
+  test("renders expected text on delete alert dialog box", async () => {
+    const deleteButton = screen.getByRole("button", { name: /Delete/i });
+    const headerText =
+      "Are you absolutely sure you want to delete the entire stock history?";
+    const warning = "Warning";
+    const warningDescription =
+      "This action cannot be undone. This will permanently delete your stock history and remove your data.";
+    const alertTitle = "Deleting this stock results in:";
+    const listItems = [
+      "Your Stock purchase history will be permanently deleted.",
+      "Stock predictions will be lost.",
+      "All stored data for Tesla stock cannot be retrieved.",
+      "To re-add Tesla stock, you'll need to enter the stock details again.",
+    ];
 
-    beforeEach(() => {
-        render(
-            <MemoryRouter>
-                <QueryClientProvider client={queryClient}>
-                    <DeleteStock
-                        ticker="Tesla"
-                        stock_id={1}
-                    />
-                    <Toaster />
-                </QueryClientProvider>
-            </MemoryRouter>
-        );
-    });
-    test("renders delete stock button correctly", () => {
-        const deleteButton = screen.getByRole("button", { name: /Delete/i });
-        expect(deleteButton).toBeInTheDocument();
-    });
-    test("renders alert dialog correctly", async () => {
-        const deleteButton = screen.getByRole("button", { name: /Delete/i });
-        expect(deleteButton).toBeInTheDocument();
-        await userEvent.click(deleteButton);
-        expect(await screen.findByText("Are you absolutely sure you want to delete the entire stock history?"))
-            .toBeInTheDocument();
-    });
-    test("renders expected text on delete alert dialog box", async () => {
-        const deleteButton = screen.getByRole("button", { name: /Delete/i });
-        const headerText = "Are you absolutely sure you want to delete the entire stock history?";
-        const warning = "Warning";
-        const warningDescription = "This action cannot be undone. This will permanently delete your stock history and remove your data.";
-        const alertTitle = "Deleting this stock results in:";
-        const listItems = ["Your Stock purchase history will be permanently deleted.",
-            "Stock predictions will be lost.",
-            "All stored data for Tesla stock cannot be retrieved.",
-            "To re-add Tesla stock, you'll need to enter the stock details again."];
+    expect(deleteButton).toBeInTheDocument();
+    await userEvent.click(deleteButton);
+    const instruction = await screen.findByText(/Please enter/i);
+    const span = instruction.querySelector("span");
 
-        expect(deleteButton).toBeInTheDocument();
-        await userEvent.click(deleteButton);
-        const instruction = await screen.findByText(/Please enter/i);
-        const span = instruction.querySelector("span");
+    expect(instruction).toBeInTheDocument();
+    expect(instruction).toHaveTextContent("Please enter");
+    expect(instruction).toHaveTextContent(
+      "to confirm the deletion is intentional."
+    );
+    expect(span).toBeInTheDocument();
+    expect(span).toHaveTextContent("Tesla");
+    expect(span).toHaveClass("font-medium");
 
-        expect(instruction).toBeInTheDocument();
-        expect(instruction).toHaveTextContent("Please enter");
-        expect(instruction).toHaveTextContent("to confirm the deletion is intentional.");
-        expect(span).toBeInTheDocument();
-        expect(span).toHaveTextContent("Tesla");
-        expect(span).toHaveClass("font-medium");
+    expect(await screen.findByText(headerText)).toBeInTheDocument();
+    expect(await screen.findByText(warning)).toBeInTheDocument();
+    expect(await screen.findByText(warningDescription)).toBeInTheDocument();
+    expect(await screen.findByText(alertTitle)).toBeInTheDocument();
+    for (const item of listItems) {
+      expect(await screen.findByText(item)).toBeInTheDocument();
+    }
+  });
+  test("testing error toast", async () => {
+    const deleteButton = screen.getByRole("button", { name: /Delete/i });
+    const invalidInput = "Tesla Corp";
+    expect(deleteButton).toBeInTheDocument();
+    await userEvent.click(deleteButton);
+    const inputPlaceHolder = screen.getByPlaceholderText("Tesla");
+    expect(inputPlaceHolder).toBeInTheDocument();
 
-        expect(await screen.findByText(headerText)).toBeInTheDocument();
-        expect(await screen.findByText(warning)).toBeInTheDocument();
-        expect(await screen.findByText(warningDescription)).toBeInTheDocument();
-        expect(await screen.findByText(alertTitle)).toBeInTheDocument();
-        for (const item of listItems) {
-            expect(await screen.findByText(item)).toBeInTheDocument();
-        }
+    await userEvent.type(inputPlaceHolder, invalidInput);
+    const confirmDeleteButton = screen.getByRole("button", {
+      name: /I understand the consequences of removing this stock./i,
     });
-    test("testing error toast", async () => {
-        const deleteButton = screen.getByRole("button", { name: /Delete/i });
-        const invalidInput = "Tesla Corp";
-        expect(deleteButton).toBeInTheDocument();
-        await userEvent.click(deleteButton);
-        const inputPlaceHolder = screen.getByPlaceholderText("Tesla");
-        expect(inputPlaceHolder).toBeInTheDocument();
+    await userEvent.click(confirmDeleteButton);
+    const toastMessage = await screen.findByText(
+      "Stock name does not match. Please enter the correct stock name to delete."
+    );
+    expect(toastMessage).toBeInTheDocument();
+  });
+  test("testing successful deletion", async () => {
+    const deleteButton = screen.getByRole("button", { name: /Delete/i });
+    const invalidInput = "Tesla";
+    expect(deleteButton).toBeInTheDocument();
+    await userEvent.click(deleteButton);
+    const inputPlaceHolder = screen.getByPlaceholderText("Tesla");
+    expect(inputPlaceHolder).toBeInTheDocument();
 
-        await userEvent.type(inputPlaceHolder, invalidInput);
-        const confirmDeleteButton = screen.getByRole("button", { name: /I understand the consequences of removing this stock./i });
-        await userEvent.click(confirmDeleteButton);
-        const toastMessage = await screen.findByText("Stock name does not match. Please enter the correct stock name to delete.");
-        expect(toastMessage).toBeInTheDocument();
+    await userEvent.type(inputPlaceHolder, invalidInput);
+    const confirmDeleteButton = screen.getByRole("button", {
+      name: /I understand the consequences of removing this stock./i,
     });
-    test("testing successful deletion", async () => {
-        const deleteButton = screen.getByRole("button", { name: /Delete/i });
-        const invalidInput = "Tesla";
-        expect(deleteButton).toBeInTheDocument();
-        await userEvent.click(deleteButton);
-        const inputPlaceHolder = screen.getByPlaceholderText("Tesla");
-        expect(inputPlaceHolder).toBeInTheDocument();
-
-        await userEvent.type(inputPlaceHolder, invalidInput);
-        const confirmDeleteButton = screen.getByRole("button", { name: /I understand the consequences of removing this stock./i });
-        await userEvent.click(confirmDeleteButton);
-        const toastMessage = await screen.findByText("Deleting stock...");
-        expect(toastMessage).toBeInTheDocument();
-    });
+    await userEvent.click(confirmDeleteButton);
+    const toastMessage = await screen.findByText("Deleting stock...");
+    expect(toastMessage).toBeInTheDocument();
+  });
 });
-
--- client/src/components/delete-stock.tsx
+++ client/src/components/delete-stock.tsx
@@ -1,13 +1,13 @@
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { IoTrash } from "react-icons/io5";
@@ -23,157 +23,157 @@ import { AlertCircle } from "lucide-react";
 import { IoCloseOutline } from "react-icons/io5";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 interface DeleteStockProps {
-    ticker?: string;
-    stock_id?: number;
+  ticker?: string;
+  stock_id?: number;
 }
 const deleteStock = async ({
-    stock_id,
-    user_id,
-    supabase,
+  stock_id,
+  user_id,
+  supabase,
 }: {
-    stock_id: number;
-    user_id: string;
-    supabase: SupabaseClient;
+  stock_id: number;
+  user_id: string;
+  supabase: SupabaseClient;
 }): Promise<void> => {
-    const { error: purchasesError } = await supabase
-        .from("User_Stock_Purchases")
-        .delete()
-        .match({ user_id: user_id, stock_id: stock_id });
-    if (purchasesError) {
-        console.error("Error deleting records:", purchasesError.message);
-    }
-    const { error: stocksError } = await supabase
-        .from("User_Stocks")
-        .delete()
-        .match({ user_id: user_id, stock_id: stock_id });
+  const { error: purchasesError } = await supabase
+    .from("User_Stock_Purchases")
+    .delete()
+    .match({ user_id: user_id, stock_id: stock_id });
+  if (purchasesError) {
+    console.error("Error deleting records:", purchasesError.message);
+  }
+  const { error: stocksError } = await supabase
+    .from("User_Stocks")
+    .delete()
+    .match({ user_id: user_id, stock_id: stock_id });
 
-    if (stocksError) {
-        console.error("Error deleting from User_Stocks:", stocksError.message);
-    }
+  if (stocksError) {
+    console.error("Error deleting from User_Stocks:", stocksError.message);
+  }
 };
 export function DeleteStock({ ticker, stock_id }: DeleteStockProps) {
-    const { user, supabase } = useSupabase();
-    const queryClient = useQueryClient();
-    const [inputValue, setInputValue] = useState("");
-    const navigate = useNavigate();
+  const { user, supabase } = useSupabase();
+  const queryClient = useQueryClient();
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
-    const mutation = useMutation({
-        mutationFn: deleteStock,
-        onSuccess: async () => {
-            await queryClient.invalidateQueries({
-                queryKey: [cache_keys.USER_STOCKS],
-            });
-            toast.success("Stock deleted successfully.");
-        },
-        onError: (error) => {
-            console.error("Error deleting stock:", error.message);
-            toast.error("Error deleting stock. Please try again.");
-        },
-    });
-    const handleDelete = async () => {
-        if (inputValue === ticker) {
-            if (stock_id === undefined || stock_id === null) {
-                console.error("Invalid stock_id");
-                toast.error("Invalid stock selection.");
-                return;
-            }
+  const mutation = useMutation({
+    mutationFn: deleteStock,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: [cache_keys.USER_STOCKS],
+      });
+      toast.success("Stock deleted successfully.");
+    },
+    onError: (error) => {
+      console.error("Error deleting stock:", error.message);
+      toast.error("Error deleting stock. Please try again.");
+    },
+  });
+  const handleDelete = async () => {
+    if (inputValue === ticker) {
+      if (stock_id === undefined || stock_id === null) {
+        console.error("Invalid stock_id");
+        toast.error("Invalid stock selection.");
+        return;
+      }
 
-            if (!user?.id) {
-                console.error("User is not authenticated");
-                toast.error("You must be logged in to delete stocks.");
-                return;
-            }
-            toast.message("Deleting stock...", { duration: 2000 });
-            mutation.mutate({
-                stock_id: stock_id ?? 0,
-                user_id: user?.id ?? "",
-                supabase,
-            });
-            await queryClient.invalidateQueries({
-                queryKey: [cache_keys.USER_STOCKS],
-            });
-            await navigate("/", { replace: true });
-        } else {
-            toast.error(
-                "Stock name does not match. Please enter the correct stock name to delete."
-            );
-            setInputValue("");
-        }
-    };
+      if (!user?.id) {
+        console.error("User is not authenticated");
+        toast.error("You must be logged in to delete stocks.");
+        return;
+      }
+      toast.message("Deleting stock...", { duration: 2000 });
+      mutation.mutate({
+        stock_id: stock_id ?? 0,
+        user_id: user?.id ?? "",
+        supabase,
+      });
+      await queryClient.invalidateQueries({
+        queryKey: [cache_keys.USER_STOCKS],
+      });
+      await navigate("/", { replace: true });
+    } else {
+      toast.error(
+        "Stock name does not match. Please enter the correct stock name to delete."
+      );
+      setInputValue("");
+    }
+  };
 
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>
-                <Button variant="delete" size="sm">
-                    {" "}
-                    <IoTrash className="" /> Delete
-                </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader className="flex justify-between items-start">
-                    <div className="flex justify-end w-full">
-                        <AlertDialogCancel
-                            className="flex justify-end w-fit"
-                            onClick={() => {
-                                setInputValue("");
-                            }}
-                        >
-                            <IoCloseOutline />
-                        </AlertDialogCancel>
-                    </div>
-                    <AlertDialogTitle>
-                        Are you absolutely sure you want to delete the entire stock history?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                        <Alert variant="destructive">
-                            <AlertCircle className="h-4 w-4" />
-                            <AlertTitle>Warning</AlertTitle>
-                            <AlertDescription>
-                                This action cannot be undone. This will permanently delete your
-                                stock history and remove your data.
-                            </AlertDescription>
-                        </Alert>
-                        <Alert variant="default" className="mt-4">
-                            <AlertTitle> Deleting this stock results in:</AlertTitle>
-                            <AlertDescription>
-                                <ul className="list-disc pl-5">
-                                    <li>
-                                        Your Stock purchase history will be permanently deleted.
-                                    </li>
-                                    <li>Stock predictions will be lost.</li>
-                                    <li>
-                                        All stored data for {ticker} stock cannot be retrieved.
-                                    </li>
-                                    <li>
-                                        To re-add {ticker} stock, you'll need to enter the stock
-                                        details again.
-                                    </li>
-                                </ul>
-                            </AlertDescription>
-                        </Alert>
-                    </AlertDialogDescription>
-                    <h2 className="text-md pt-3 border-t-2">
-                        Please enter <span className="font-medium">{ticker} </span>to
-                        confirm the deletion is intentional.
-                    </h2>
-                    <Input
-                        type="text"
-                        placeholder={ticker}
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                    />
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogAction
-                        onClick={handleDelete}
-                        className="bg-[#e50000] text-[#e50000]-foreground shadow
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="delete" size="sm">
+          {" "}
+          <IoTrash className="" /> Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader className="flex justify-between items-start">
+          <div className="flex justify-end w-full">
+            <AlertDialogCancel
+              className="flex justify-end w-fit"
+              onClick={() => {
+                setInputValue("");
+              }}
+            >
+              <IoCloseOutline />
+            </AlertDialogCancel>
+          </div>
+          <AlertDialogTitle>
+            Are you absolutely sure you want to delete the entire stock history?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Warning</AlertTitle>
+              <AlertDescription>
+                This action cannot be undone. This will permanently delete your
+                stock history and remove your data.
+              </AlertDescription>
+            </Alert>
+            <Alert variant="default" className="mt-4">
+              <AlertTitle> Deleting this stock results in:</AlertTitle>
+              <AlertDescription>
+                <ul className="list-disc pl-5">
+                  <li>
+                    Your Stock purchase history will be permanently deleted.
+                  </li>
+                  <li>Stock predictions will be lost.</li>
+                  <li>
+                    All stored data for {ticker} stock cannot be retrieved.
+                  </li>
+                  <li>
+                    To re-add {ticker} stock, you'll need to enter the stock
+                    details again.
+                  </li>
+                </ul>
+              </AlertDescription>
+            </Alert>
+          </AlertDialogDescription>
+          <h2 className="text-md pt-3 border-t-2">
+            Please enter <span className="font-medium">{ticker} </span>to
+            confirm the deletion is intentional.
+          </h2>
+          <Input
+            type="text"
+            placeholder={ticker}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            onClick={handleDelete}
+            className="bg-[#e50000] text-[#e50000]-foreground shadow
                      hover:bg-[#e50000]/90 text-white border-1dark:hover:border-white hover:border-black hover:border-2
                       dark:active:bg-[#e50000]/40 active:bg-[#e50000]/40 w-full"
-                    >
-                        I understand the consequences of removing this stock.
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    );
+          >
+            I understand the consequences of removing this stock.
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
