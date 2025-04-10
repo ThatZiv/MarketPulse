88e711bb2b22840a8d463689ff6b18e7c85ac5fc
IanCro12 <ft5139@wayne.edu>
2025-04-10 13:42:19 -0400

    Fixed CSS

--- client/src/pages/StockSelection.tsx
+++ client/src/pages/StockSelection.tsx
@@ -576,27 +576,7 @@ export default function StockPage() {
                       </label>
                     )}
 
-                      <div>
-                        <input
-                          id={`date-${index}`}
-                          type="datetime-local"
-                          required
-                          value={purchase.date}
-                          min="2000-01-01T00:00"
-                          max={formatToday()}
-                          onChange={(e) => {
-                            handlePurchaseChange(
-                              index,
-                              "date",
-                              moment(e.target.value).format("YYYY-MM-DDTHH:mm")
-                            );
-                          }}
-                          onBlur={() => {
-                            handleSort(index);
-                          }}
-                          className={`border border-gray-300 bg-white text-white dark:text-black dark:bg-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary ${window.innerWidth > 600 ? "max-w-14" : ""}`}
-                        />
-                      </div>
+                    <div>
                       <input
                         id={`date-${index}`}
                         type="datetime-local"
@@ -614,31 +594,9 @@ export default function StockPage() {
                         onBlur={() => {
                           handleSort(index);
                         }}
-                        className="border border-gray-300 bg-white text-black dark:text-white dark:bg-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
+                        className={`border border-gray-300 bg-white text-white dark:text-black dark:bg-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary ${window.innerWidth < 600 ? "max-w-14" : ""}`}
                       />
-                    ) : (
-                      <div>
-                        <input
-                          id={`date-${index}`}
-                          type="datetime-local"
-                          required
-                          value={purchase.date}
-                          min="2000-01-01T00:00"
-                          max={formatToday()}
-                          onChange={(e) => {
-                            handlePurchaseChange(
-                              index,
-                              "date",
-                              moment(e.target.value).format("YYYY-MM-DDTHH:mm")
-                            );
-                          }}
-                          onBlur={() => {
-                            handleSort(index);
-                          }}
-                          className="border border-gray-300 bg-white text-white dark:text-black dark:bg-black rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary max-w-14"
-                        />
-                      </div>
-                    )}
+                    </div>
                   </div>
 
                   <div className="flex-1 flex flex-col min-w-20">
