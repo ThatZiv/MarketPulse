5e2ffe239e9da82552e1a228504dc3f0f34a177f
ThatZiv <thatziv@gmail.com>
2025-04-13 18:35:17 -0400

    fix auto exit backend

--- server/models/sentiment_hf.py
+++ server/models/sentiment_hf.py
@@ -4,14 +4,13 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-ADD_1 = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
-ADD_2 = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
-tokenizer = AutoTokenizer.from_pretrained(ADD_1)
-model = AutoModelForSequenceClassification.from_pretrained(ADD_2)
-
 
 # calls the model distilroberta-finetuned-financial-news-sentiment-analysis for one text input
 def sentiment_model(text):
+    ADD = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+    tokenizer = AutoTokenizer.from_pretrained(ADD)
+    model = AutoModelForSequenceClassification.from_pretrained(ADD)
+
     inputs = tokenizer(text, return_tensors="pt")
     with torch.no_grad():
         logits = model(**inputs).logits
