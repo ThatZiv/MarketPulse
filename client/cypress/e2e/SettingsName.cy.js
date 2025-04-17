3fc24d36c6332eaadcb67662cfe2e479307919ae
IanCro12 <ft5139@wayne.edu>
2025-04-17 02:01:56 -0400

    Test change

--- client/cypress/e2e/SettingsName.cy.js
+++ client/cypress/e2e/SettingsName.cy.js
@@ -25,9 +25,11 @@ describe("Change first and last name", () => {
 
     cy.get("span").contains("Settings").click();
     cy.wait(200);
-    cy.get('input[name="first_name"]').type("{selectAll}").type("John");
+    cy.get('input[name="first_name"]').clear();
+    cy.get('input[name="first_name"]').type("John");
     cy.wait(200);
-    cy.get('input[name="last_name"]').type("{selectAll}").type("Doe");
+    cy.get('input[name="last_name"]').clear();
+    cy.get('input[name="last_name"]').type("Doe");
 
     cy.wait(50);
     cy.get("button").contains("Save Changes").click();
@@ -52,10 +54,11 @@ describe("Change first and last name", () => {
 
     cy.get("span").contains("Settings").click();
     cy.wait(200);
-    cy.get('input[name="first_name"]').type("{selectAll}").type("Bill");
+    cy.get('input[name="first_name"]').clear();
+    cy.get('input[name="first_name"]').type("Bill");
     cy.wait(200);
-    cy.get('input[name="last_name"]').type("{selectAll}").type("Smith");
-
+    cy.get('input[name="last_name"]').clear();
+    cy.get('input[name="last_name"]').type("Smith");
     cy.wait(50);
     cy.get("button").contains("Save Changes").click();
 
